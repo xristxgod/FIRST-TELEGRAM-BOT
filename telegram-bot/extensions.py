0a3f560820ea344f9d427e99e7acbe8486971d86
Murad <mamedov_99b@mail.ru>
2021-06-05 14:58:21 +0300

    Telegram-bot v02

--- telegram-bot/extensions.py
+++ telegram-bot/extensions.py
@@ -1,31 +1,54 @@
+from datetime import datetime
+
 import requests
 import json
+import lxml.html
 
-import config as cnf
-
+from collections import defaultdict
 
 class ConvertionExceprion(Exception):
     pass
+
+class UserInfo:
+
+    ''' Информация о пользователя '''
+
+    def __init__(self):
+        self.f = "RUB"
+        self.t = "USD"
+
+class UserDB:
+
+    ''' База даных для пользователий '''
+
+    def __init__(self):
+        self.db = defaultdict(UserInfo)
+
+    def change_from(self, user_id, val):
+        self.db[user_id].f = val
+
+    def change_too(self, user_id, val):
+        self.db[user_id].t = val
+
+    def get_pair(self, user_id):
+        user = self.db[user_id]
+        return user.f, user.t
 
 class CryptoConverter:
 
     ''' Обработка исключений функции get_values '''
 
     @staticmethod
-    def convert(quote: str, base: str, amount: str):
+    def convert(values):
+        if len(values) != 3:
+            raise ConvertionExceprion('Слишком много параметров')
+        quote, base, amount = values
 
         if quote == base:
             raise ConvertionExceprion(f'Невозможно перевести одинкаовые валюты {base}')
 
-        try:
-            quote_ticker = cnf.KEYS[quote]
-        except KeyError:
-            raise ConvertionExceprion(f'Не удалось найти валюту {quote}')
-
-        try:
-            base_ticker = cnf.KEYS[base]
-        except KeyError:
-            raise ConvertionExceprion(f'Не удалось найти валюту {base}')
+        quote_ticker = quote
+        base_ticker = base
 
         try:
             amount = float(amount)
@@ -33,11 +56,27 @@ class CryptoConverter:
             raise ConvertionExceprion(f'Не удалось обработать количество {amount}')
 
         r = requests.get(f'https://min-api.cryptocompare.com/data/price?fsym={quote_ticker}&tsyms={base_ticker}')
-        total_base = json.loads(r.content)[cnf.KEYS[base]]
+        total = float(json.loads(r.content)[base_ticker])*amount
 
-        return total_base*amount
+        return round(total, 3)
 
+class VGPrice:
 
+    @staticmethod
+    def get_vg_price():
+        html = requests.get('https://quote.rbc.ru/ticker/306109').content
+        tree = lxml.html.document_fromstring(html)
+        vg = tree.xpath(
+            '/html/body/div[7]/div[4]/div[2]/div[1]/div[3]/div/div[1]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/span[1]/text()'
+                        )[1].replace(',', '.')
+        current_datetime = datetime.now().date()
+        lifting_percentage = tree.xpath(
+            '/html/body/div[7]/div[4]/div[2]/div[1]/div[3]/div/div[1]/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/span[2]/text()'
+        )
+        lifting_percentage = '\n'.join(lifting_percentage).replace(' ', '').split()
+        lifting_percentage = ''.join(lifting_percentage)
+        text = f'Цена Virgin Galactic на {current_datetime} состовляет - {vg} USD {lifting_percentage}'
+        return text
 
 
 if __name__ == '__main__':
--- telegram-bot/main_bot.py
+++ telegram-bot/main_bot.py
@@ -1,60 +1,114 @@
+import datetime
+
 import telebot
 import requests
 import json
+import lxml.html
 
 import config as cnf
+from extensions import (
+    ConvertionExceprion,
+    CryptoConverter,
+    UserDB,
+    VGPrice
+)
 
-
-from extensions import ConvertionExceprion, CryptoConverter
 
 # Подключаем бота
 bot = telebot.TeleBot(cnf.TOKEN)
 
-
-
+DB = UserDB()
 
 @bot.message_handler(commands=['start', 'help'])
 def start_or_help(message: telebot.types.Message):
 
-    ''' Ответ на команды /help и /start '''
+    ''' Функция команд: /help и /start '''
 
     text = 'Список всех доступных валют: /values \n' \
-           'Что бы узнать цену валюты в другой валюте:  ' \
-           '<имя валюты, цену которой нужно узнать> <имя валюты, в которой надо перевести> <количество первой валюты>' \
+           'Конвертировать валюты: /set \n' \
+           'Узнать цену акций Virgin Galactic: /get \n'
 
     bot.send_message(message.chat.id, text)
+
+@bot.message_handler(commands=['get'])
+def get_vg(message: telebot.types.Message):
+    try:
+        text = VGPrice.get_vg_price()
+    except Exception as e:
+        bot.send_message(message.chat.id, e)
+    else:
+        bot.send_message(message.chat.id, text)
 
 @bot.message_handler(commands=['values'])
 def get_all_values(message: telebot.types.Message):
 
-
-    ''' Ответ на команду /values '''
+    ''' Функция команды: /values '''
 
     text = 'Доступные валюты: \n'
-    for key in cnf.KEYS:
-        text = '\n'.join((text, key, ))
+    text += '\n'.join(f'{i+1}. {key}' for i, key in enumerate(cnf.KEYS))
     bot.send_message(message.chat.id, text)
+
+@bot.message_handler(commands=['set'])
+def set(message: telebot.types.Message):
+
+    ''' Функция команды: /set '''
+    ''' Вывод двух Keyboards со списками валют'''
+
+    markup = telebot.types.InlineKeyboardMarkup()
+    for val, form in cnf.KEYS.items():
+        button = telebot.types.InlineKeyboardButton(text=val, callback_data=f'val1 {form}')
+        markup.add(button)
+
+    bot.send_message(chat_id=message.chat.id, text='Выберите валюту, из которой будем конвертировать', reply_markup=markup)
+
+    markup = telebot.types.InlineKeyboardMarkup()
+    for val, form in cnf.KEYS.items():
+        button = telebot.types.InlineKeyboardButton(text=val, callback_data=f'val2 {form}')
+        markup.add(button)
+
+    bot.send_message(chat_id=message.chat.id, text='Выберите валюту, в которую будем конвертировать',
+                     reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def get_value(call):
+
+    ''' Функция команды: /set '''
+    ''' Реакция на изменения валюты '''
+
+    t, st = call.data.split()
+    user_id = call.message.chat.id
+    if t == 'val1':
+        DB.change_from(user_id, st)
+    if t == 'val2':
+        DB.change_too(user_id, st)
+
+    pair = DB.get_pair(user_id)
+    bot.answer_callback_query(callback_query_id=call.id, show_alert=True, text=f'Теперь конвертируем из {pair[0]} в {pair[1]}')
 
 @bot.message_handler(content_types=['text', ])
 def get_values(message: telebot.types.Message):
 
-    ''' Вывод цены криптовалюты в нужной валюте '''
+    ''' Функция команды: /set '''
+    ''' Вывод цены валюты '''
 
+    pair = DB.get_pair(message.chat.id)
+    values = [*pair, message.text.strip()]
     try:
-        values = message.text.title().split(' ')
-        if len(values) != 3: raise ConvertionExceprion('Слишком много параметров')
-        quote, base, amount = values
-        total_base = CryptoConverter.convert(quote, base, amount)
+
+        total = CryptoConverter.convert(values)
+
     except ConvertionExceprion as c:
         bot.send_message(message.chat.id, f'Ошибка пользователя. \n{c}')
     except Exception as e:
         bot.send_message(message.chat.id, f'Не удалось обработать команду\n{e}')
     else:
-        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        text = f'{values[2]} {values[0]} -- {round(total, 2)} {values[1]}'
         bot.send_message(message.chat.id, text)
 
+
 # Run bot
-bot.polling(none_stop=True)
+
+bot.polling(none_stop=True, interval=0)
 
 
 
